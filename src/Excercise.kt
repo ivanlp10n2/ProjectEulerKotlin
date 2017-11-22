35273cee8499437d5c2841ecadc40e117283ca54
Monzon <ivan.monzon@accenture.com>
2017-11-22 15:46:21 -0300

    first commit

---
+++ src/Excercise.kt
@@ -0,0 +1,13 @@
+import jdk.nashorn.internal.objects.Global.println
+
+class Excercise {
+
+    fun doMe(): Int {
+        for (i in 1..9) {
+            println(i)
+        }
+
+        return 3
+    }
+
+}
---
+++ src/excercise_1/Multiples3and5.kt
@@ -0,0 +1,10 @@
+package excercise_1
+
+fun main(args: Array<String>) {
+
+    for (i in 0..10){
+        i += 2
+        println(i)
+    }
+
+}
