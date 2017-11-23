90619565c68843b7f1e6b468d152e1524f74e5bf
Monzon <ivan.monzon@accenture.com>
2017-11-23 13:00:12 -0300

    second excercise_ok

--- src/excercise_2/Even Fibonacci numbers.kt
+++ src/excercise_2/Even Fibonacci numbers.kt
@@ -1,6 +1,25 @@
 package excercise_2
 
-fun main(args: Array<String>)
-{
+fun main(args: Array<String>) {
+
+    var fibonacci_sum = 1
+    var fibonacci_even_sum = 0
+
+    var first = 1
+    var second = 1
+
+    /*Probably not the best solution, but it works*/
+
+    while ( fibonacci_sum < 4000000 )
+    {
+        println(fibonacci_sum)
+        if (fibonacci_sum % 2 == 0) fibonacci_even_sum += fibonacci_sum
+        fibonacci_sum = first + second
+        first = second
+        second = fibonacci_sum
+    }
+
+    println("The result is " + fibonacci_even_sum)
+
 
 }
